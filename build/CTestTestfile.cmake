08b8e95da0c11df3df7ac9c4cea62b1cb2024d5b
Federico Dessì <dessi.fed@gmail.com>
2020-04-11 19:00:25 +0200

    Removed unnecessary files

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/peterbrown/Scrivania/UNI/TERZO ANNO/Linguaggi e tecnologie per il web/ProgettoLTW
-# Build directory: /home/peterbrown/Scrivania/UNI/TERZO ANNO/Linguaggi e tecnologie per il web/ProgettoLTW/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
