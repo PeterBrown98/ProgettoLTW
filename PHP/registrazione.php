3b1b234edf6ead953bedd845d1a5446095ca5f97
Jonny <jonnygates62@gmail.com>
2020-05-01 16:35:21 +0200

    aggiornato php

--- PHP/registrazione.php
+++ PHP/registrazione.php
@@ -10,10 +10,10 @@
             echo "Le password immesse non coincidono, riprova!";
         }
         else{
-            $dbconn = pg connect(”host=localhost port=5433 dbname=foodream user=pc password=foodream”)
-                        or die(’ Could not connect : ’ . pg last error());
+            $dbconn = pg_connect("host=localhost port=5433 dbname=foodream user=pc password=foodream")
+                        or die('Could not connect : ' . pg_last_error());
             $query = "insert into utente(nome,cognome,email,password) values (" . "$nome" . "," . "$cognome" . "," . "$email" . "," . "$password" . ")";
-            pg query($query);
+            pg_query($query);
             echo 'Registrazione avvenuta con successo.';
         }
 
