49d9094e1b92a855cab1094ffda1be7f814b06b3
Jonny <jonnygates62@gmail.com>
2020-05-03 17:23:51 +0200

    resa funzionante pagina di login, migliorato il funzionamento della pagina di registrazione, eliminato file registrazione.php sostituito da registrazione2.php e creata la pagina welcome.php.

---
+++ PHP/login.php
@@ -0,0 +1,39 @@
+<html">
+    <head></head>
+    <body>
+        <?php
+            $dbconn = pg_connect("host=localhost port=5433 dbname=dbfoodream user=postgres password=postgres")
+            or die('Could not connect : ' . pg_last_error());
+            
+            if(!(isset($_POST["accedi"]))){header("Location: ../index.html");}
+            else{
+                $email=$_POST['email'];
+                $q1="select * from utente where email=$1";
+                $result=pg_query_params($dbconn, $q1, array($email));
+                if(!($line=pg_fetch_array($result,null,PGSQL_ASSOC))){
+                    echo "<h1> Non sei ancora registrato su Foodream? Nessun problema </h1>
+                    <a href=../registrazione2.html>registrati ora</a>";
+                }
+                else{
+                    $password=md5($_POST['password']);
+                    $q2="select * from utente where email=$1 and password=$2";
+                    $result= pg_query_params($dbconn, $q2, array($email, $password));
+                    if(!($line=pg_fetch_array($result,null,PGSQL_ASSOC))){
+                        echo "<h1> Password errata, </h1>
+                        <a href=../login.html>riprova</a>";
+                    }
+                    else{
+                        $nome=$line['nome'];
+                        echo "<a href=welcome.php?name=$nome>Premi qui</a> per utilizzare il sito.";
+                    }
+                }
+              
+            }
+            
+            
+            
+            
+            ?>
+
+    </body>
+</html>
