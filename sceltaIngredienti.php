2ea47da3ddc2bae405637a59241fd719c7bc187c
eric25010 <enrico.graziani12@gmail.com>
2020-05-08 21:39:39 +0200

    fottuto pg_connect

--- sceltaIngredienti.php
+++ sceltaIngredienti.php
@@ -62,13 +62,13 @@
                 <br>
                 <input class="awesomplete" list="mylist" placeholder = "Seleziona un ingrediente"/>
                 <datalist id="mylist">
-                                <?php
-                                    $db = pg-connect("host=localhost, port=5433, dbname=dbfoodream user=postgres password=postgres");
-                                    $result1 = pg_query($db, "SELECT nome FROM ingrediente order by nome ASC");
-                                    while ($row = pg_fetch_row($result1)) {
-                                    echo "<option value='$row[0]'>$row[0]</option>";
-                                    }
-                                ?>
+                    <?php
+                        $db = pg_connect("host=localhost, port=5433, dbname=dbfoodream user=postgres password=postgres");
+                        $result1 = pg_query($db, "SELECT nome FROM ingrediente order by nome ASC");
+                        while ($row = pg_fetch_row($result1)) {
+                        echo "<option value='$row[0]'>$row[0]</option>";
+                        }
+                    ?>
                 </datalist>
         </main>
         <footer id="sticky-footer" class="py-4  text-white">
