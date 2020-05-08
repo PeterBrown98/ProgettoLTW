04149dac70408d08ad81b283101fa58b4a618d31
eric25010 <enrico.graziani12@gmail.com>
2020-05-08 21:06:05 +0200

    correzione

--- sceltaIngredienti.php
+++ sceltaIngredienti.php
@@ -73,13 +73,15 @@
                     <div id="suggesstion-box"></div>
                 </div>
             </section>
-            <input class="awesomplete" list="mylist" />
+            <input class="awesomplete" list="mylist" placeholder = "Seleziona un ingrediente"/>
             <datalist id="mylist">
                 <?php
                     $db = pg-connect("host=localhost, port=5433, dbname=dbfoodream user=postgres password=postgres");
                     $result1 = pg_query($db, "SELECT nome FROM ingrediente order by nome ASC");
                     while ($row = pg_fetch_row($result1)) {
                     echo "<option value='$row[0]'>$row[0]</option>";
+                    echo "<h1>ciao</h1>";
+                    }
                 ?>
             </datalist>
         </main>
