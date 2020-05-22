4954c5d1e97db1d5a8007d5e1e175ef23efff35b
eric25010 <enrico.graziani12@gmail.com>
2020-05-22 18:36:08 +0200

    aggiunta funzione jquery in sceltaIngredienti più modifiche varie

--- PHP/sceltaIngredienti.php
+++ PHP/sceltaIngredienti.php
@@ -10,6 +10,13 @@
         <meta name="viewport" content="width-device-width, initial-scale=1"/>
         <meta name="viewport" content="height=device-height, initial-scale=1"/> 
         <script src="awesomplete.js" async></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script>
+        $(document).ready(function(){
+            $(".awesomplete").change(function() {
+            $(this).css('background-color','green');
+        })
+        </script>
 
     </head>
     <body>
@@ -50,7 +57,7 @@
             echo '<h1 class="title">Scegli gli ingredienti che vuoi usare</h1>';
             echo '<section class="ricerca">';
                 echo "<form action='ricette.php?tipoPiatto=$tipo'method='post'>";
-                echo '<input class="awesomplete" name="i1" list="mylist" placeholder = "Seleziona un ingrediente"/>';
+                echo '<input class="awesomplete" id="ing1" name="i1" list="mylist" placeholder = "Seleziona un ingrediente"/>';
                 echo "<br>";
                 echo '<input class="awesomplete" name="i2" list="mylist" placeholder = "Seleziona un ingrediente"/>';
                 echo "<br>";
@@ -71,8 +78,15 @@
                         }
                     
                         echo "</datalist>";
-                        echo '<button type="submit" class="btn-primary" id="srchbtn" name="cerca">Cerca Ricetta</button>';
+                        
+                        echo '<button type="submit" class="btn-primary" id="srchbtn" name="cerca" >Cerca Ricetta</button>';
                         echo "</form>";
+
+                        echo"<script>
+                        $('.awesomplete').change(function() {
+                            $(this).css('background-color','green')
+                        })
+                        </script>";
                         
         echo " </main>";
         echo '<footer id="sticky-footer" class="py-4  text-white">';
@@ -80,7 +94,7 @@
         echo 'Copyright &copy;<script>document.write(new Date().getFullYear());</script>,  Foodream';
         echo "</div>";
         echo "</footer>";
-        echo '<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>';
+        echo '<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>';
         echo '<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>';
         echo '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>';
      ?>         
